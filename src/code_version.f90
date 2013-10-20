ec82a8983df1714f43ecfe8efa0f69988d0c8327
MvdS <git@vandersluys.nl>
2013-10-20 18:06:22 +0200

    Updated INSTALL for separate data tarball; minor update in CMake

--- src/code_version.f90
+++ src/code_version.f90
@@ -16,8 +16,8 @@ contains
   subroutine print_libTheSky_version(unit)
     implicit none
     integer, intent(in) :: unit
-    character :: libTheSky_version*(99) = 'rev.18, hash 1fcc3ad (2013-10-20)'
-    character :: compile_date*(99) = 'Sun Oct 20 14:18:53 CEST 2013'
+    character :: libTheSky_version*(99) = 'rev.19, hash ba15397 (2013-10-20)'
+    character :: compile_date*(99) = 'Sun Oct 20 17:00:25 CEST 2013'
     character :: compiler*(99) = 'gfortran'
     character :: compiler_flags*(99) = '-O2'
     
