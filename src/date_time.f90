ba28b44f7b90542a7851715ed23ac0bcee7e5e75
Marc van der Sluys <git@vandersluys.nl>
2023-12-26 12:31:06 +0100

    Change licence.

--- src/date_time.f90
+++ src/date_time.f90
@@ -5,15 +5,16 @@
 !   
 !  This file is part of the libTheSky package, 
 !  see: http://libthesky.sf.net/
-!   
-!  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
-!  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 !  
-!  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-!  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+!  This is free software: you can redistribute it and/or modify it under the terms of the
+!  European Union Public Licence 1.2 (EUPL 1.2).
 !  
-!  You should have received a copy of the GNU General Public License along with this code.  If not, see 
-!  <http://www.gnu.org/licenses/>.
+!  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+!  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+!  See the EU Public Licence for more details.
+!  
+!  You should have received a copy of the European Union Public Licence along with this code.
+!  If not, see <https://www.eupl.eu/1.2/en/>.
 
 
 !  set_date_and_time:                   Set global date/time variables (year, month, ..., minute, second) to specified values
--- src/libTheSky.f90
+++ src/libTheSky.f90
@@ -1,29 +1,30 @@
 !> \file libTheSky.f90  Used for documentation purposes
 !!  
 !!  \mainpage libTheSky documentation
-!!  libTheSky is a LIBrary to compute the positions of Sun, Moon, planets and more in The Sky.  libTheSky is available for free
-!!  under the conditions of the GPL v.3.
+!!  libTheSky is a LIBrary to compute the positions of Sun, Moon, planets and more in The Sky.  libTheSky is
+!!  available for free under the conditions of the EUPL v.1.2.
 !!  
-!!  \par
-!!  Copyright &copy; 2002-2020  Marc van der Sluys - <a href="http://marc.vandersluys.nl">marc.vandersluys.nl</a>
+!!  \par  
+!!  Copyright &copy; 2002-2023 Marc van der Sluys - <a
+!!  href="http://marc.vandersluys.nl">marc.vandersluys.nl</a>
 !!
 !!  \see
 !!  These pages contain the documentation for libTheSky, created by Doxygen.  
-!!  For the libTheSky home page, see: http://libthesky.sf.net
+!!  For the libTheSky home page, see: https://libthesky.sf.net
 
 
 !  Copyright (c) 2002-2023  Marc van der Sluys - marc.vandersluys.nl
 !   
 !  This file is part of the libTheSky package, 
-!  see: http://libthesky.sf.net/
-!   
-!  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
-!  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+!  see: https://libthesky.sf.net/
 !  
-!  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-!  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+!  This is free software: you can redistribute it and/or modify it under the terms of the
+!  European Union Public Licence 1.2 (EUPL 1.2).
 !  
-!  You should have received a copy of the GNU General Public License along with this code.  If not, see 
-!  <http://www.gnu.org/licenses/>.
-
+!  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+!  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+!  See the EU Public Licence for more details.
+!  
+!  You should have received a copy of the European Union Public Licence along with this code.
+!  If not, see <https://www.eupl.eu/1.2/en/>.
 
